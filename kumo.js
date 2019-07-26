356c84c113cb61f66a54e69bf462b008abf8ae81
seekr <mario.drs@gmail.com>
2019-07-25 23:26:17 -0300

    Update kumo.js

--- kumo.js
+++ kumo.js
@@ -11,25 +11,18 @@ const h = window.location.hash
 const x = !h ? 20 : +h.substr(1)
 const c = x <= 500 ? x : 500
 
-const {appVersion, userAgent} = navigator
-if (!!~appVersion.indexOf('Win')
-  || !!~userAgent.indexOf('MSIE')
-  || !!~userAgent.indexOf('Win')) {
-  alert('Nope')
-} else {
-  fetch(`${H}/topstories.json`)
-    .then(r => r.json())
-    .then(d => {
-      for (let i = 0; i < c; i++) {
-        fetch(`${H}/item/${d[i]}.json`)
-          .then(r => r.json())
-          .then(({title, url}) => {
-            document.body.append(Object.assign(document.createElement('a'), {
-              innerHTML: title,
-              target: '_blank',
-              href: url
-            }))
-          })
-      }
-    })
-}
+fetch(`${H}/topstories.json`)
+  .then(r => r.json())
+  .then(d => {
+    for (let i = 0; i < c; i++) {
+      fetch(`${H}/item/${d[i]}.json`)
+        .then(r => r.json())
+        .then(({title, url}) => {
+          document.body.append(Object.assign(document.createElement('a'), {
+            innerHTML: title,
+            target: '_blank',
+            href: url
+          }))
+        })
+    }
+  })

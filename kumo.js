c801a45da4a03049b81237a35cfd9c8cd41e5919
seekr <mario.drs@gmail.com>
2019-10-14 16:55:55 -0300

    update from 9e70d2e6
    https://gitlab.com/joshavanier/kumo/commit/9e70d2e68aa68878b57d99b03f15dc6303af88a5

--- kumo.js
+++ kumo.js
@@ -1,28 +1,55 @@
-/**
- * クモ
- * HN client
- *
- * @author Josh Avanier
- * @license MIT
- */
+'use strict';
 
-const H = 'https://hacker-news.firebaseio.com/v0'
-const h = window.location.hash
-const x = !h ? 20 : +h.substr(1)
-const c = x <= 500 ? x : 500
+const API = 'https://hacker-news.firebaseio.com/v0';
+const defCount = 20;
+const maxCount = 500;
+const {hash} = window.location;
+const hashVal = !hash ? defCount : +hash.substr(1);
+const count = isNaN(hashVal)
+  ? defCount : hashVal <= maxCount
+    ? hashVal : maxCount;
 
-fetch(`${H}/topstories.json`)
-  .then(r => r.json())
-  .then(d => {
-    for (let i = 0; i < c; i++) {
-      fetch(`${H}/item/${d[i]}.json`)
-        .then(r => r.json())
-        .then(({title, url}) => {
-          document.body.append(Object.assign(document.createElement('a'), {
-            innerHTML: title,
-            target: '_blank',
-            href: url
-          }))
-        })
+let loadCount = 0;
+
+function isShit (text, filter = topics) {
+  for (let i = 0, l = filter.length; i < l; i++) {
+    if (text.includes(filter[i])) return true;
+  }
+  return false;
+}
+
+function addStory ({title, url}) {
+  if (
+    url === undefined
+    || isShit(title.toLowerCase())
+    || isShit(url, sites)
+  ) return;
+  document.body.append(
+    Object.assign(document.createElement('a'), {
+      innerHTML: title,
+      target: '_blank',
+      href: url
+    })
+  );
+  loadCount++;
+  document.title = `クモ : ${loadCount}/${count}`;
+}
+
+fetch(`${API}/topstories.json`)
+  .then(response => response.json())
+  .then((stories) => {
+    for (let i = 0; i < count; i++) {
+      const id = stories[i];
+      if (Object.prototype.hasOwnProperty.call(sessionStorage, id)) {
+        addStory(JSON.parse(sessionStorage.getItem(id)));
+        continue;
+      }
+      fetch(`${API}/item/${id}.json`)
+        .then(data => data.json())
+        .then((result) => {
+          const story = {title: result.title, url: result.url};
+          sessionStorage.setItem(id, JSON.stringify(story));
+          addStory(story);
+        });
     }
-  })
+  });

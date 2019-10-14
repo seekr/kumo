3ff20cbcd089ff7f887ee01526f51808cc2f80d6
seekr <mario.drs@gmail.com>
2019-10-14 16:56:32 -0300

    Create filter.js

---
+++ filter.js
@@ -0,0 +1,33 @@
+const topics = `
+.net
+amazon
+bezos
+bing
+bootstrap
+c#
+facebook
+github
+marketing
+microsoft
+ms-dos
+nat friedman
+satya nadella
+skype
+startup
+trump
+vbscript
+visual basic
+visual studio
+windows
+xbox
+zuckerberg
+`.trim().split('\n');
+
+const sites = `
+amazon.com
+bloomberg.com
+newyorker.com
+qz.com
+theverge.com
+vox.com
+`.trim().split('\n');
